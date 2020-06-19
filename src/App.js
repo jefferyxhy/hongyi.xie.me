14d11c0cdcabb6edaf2c794ba7f7ab4f658c42ed
Hongyi Xie <xhyadelaide@gmail.com>
2020-06-20 02:07:46 +0930

    add basic introduction & about component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Introduction from './components/Introduction';
+import SideBar from './components/SideBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="colorlib-page">
+        <div id="container-wrap">
+          <SideBar></SideBar>
+				<div id="colorlib-main">
+					<Introduction></Introduction>
+          <About></About>
+					{/*
+					<Timeline></Timeline> */}
+          	</div>
+      	</div>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+
+export class SideBar extends Component {
+  render() {
+    return (
+      <div>
+        <a href="#" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <i />
+        </a>
+        <aside id="colorlib-aside" role="complementary" className="border js-fullheight">
+          <div className="text-center">
+            <div className="author-img" style={{ backgroundImage: "url(images/about.jpg)" }} />
+            <h1 id="colorlib-logo">
+              <a href="index.html">Hongyi Xie</a>
+            </h1>
+            <span className="position">
+              <a href="#">Full Stack Web Developer</a> in Australia
+            </span>
+          </div>
+          <nav id="colorlib-main-menu" role="navigation" className="navbar">
+            <div id="navbar" className="collapse">
+              <ul>
+                <li className="active">
+                  <a href="#" data-nav-section="home">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#" data-nav-section="about">
+                    About
+                  </a>
+                </li>
+                {/* <li>
+                  <a href="#" data-nav-section="skills">
+                    Skills
+                  </a>
+                </li>
+                <li>
+                  <a href="#" data-nav-section="education">
+                    Education
+                  </a>
+                </li>
+                <li>
+                  <a href="#" data-nav-section="experience">
+                    Experience
+                  </a>
+                </li>
+                <li>
+                  <a href="#" data-nav-section="work">
+                    Work
+                  </a>
+                </li>
+                <li>
+                  <a href="#" data-nav-section="contact">
+                    Contact
+                  </a>
+                </li> */}
+              </ul>
+            </div>
+          </nav>
+          <div role="navigation" className="navbar">
+            <div>
+              <a href="https://github.com/jefferyxhy">
+                <i className="icon-github" />
+              </a>
+            </div>
+            <div>
+              <a href="https://www.linkedin.com/in/hongyi-xie-091359b8/">
+                <i className="icon-linkedin2" />
+              </a>
+            </div>
+          </div>
+        </aside>
+      </div>
+    );
+  }
+}
+
+export default SideBar;
